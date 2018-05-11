deac3633fbc471be094d42bbabbe63f43124336a
raquelmv <raquel.mvillarreal@gmail.com>
2018-05-10 21:29:02 -0500

    dynamic buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+//------
+$(document).ready(function() {
+//---- dynamic buttons---//
+    function createButtons() {
+        var buttonTexts = [ "1","2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "/", "^", "="];
+        var calculatorHolder = $("#calculator-holder");
+
+        for (index in buttonTexts) {
+            console.log(index);
+            var button = $("<button>");
+            button.attr("data-letter", buttonTexts[index]);
+            calculatorHolder.html(button);
+        }
+    }
+
+    createButtons();
+});
